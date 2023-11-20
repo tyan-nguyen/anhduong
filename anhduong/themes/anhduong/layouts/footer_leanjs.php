aab7b7dddb605bc608b974dbdab01622b996a3b5
tyan-nguyen <nguyenvantyan@gmail.com>
2023-11-20 19:37:13 +0700

    update

--- anhduong/themes/anhduong/layouts/footer_leanjs.php
+++ anhduong/themes/anhduong/layouts/footer_leanjs.php
@@ -1,31 +1,27 @@
 <footer id="footer" class="footer solid-bg">
-
 		
-		
-<div class="custom-footer" style="padding:80px 50px 80px 100px">
+<div class="custom-footer">
       <div class="custom-footer-logo">
         <img src="/anhduong/images/logo.png" alt="logo">
       </div>
       <div class="custom-footer-address">
         <div>
           <p class="custom-footer-address-title">Văn Phòng</p>
-          <div style="width: 50px; height: 3px; background-color: #ffffff; margin-bottom:20px"></div>
-          <p>
-            <img src="/anhduong/images/location.svg" alt="location">
-            <span>40/3 Khu phố Bình Giao, Phường Thuận Giao, Thành phố Thuận An, Tỉnh Bình Dương</span>
+          <div style="width: 150px; height: 3px; background-color: #ffffff; margin-bottom:20px"></div>
+          <p>           	
+            <span><i class="fa-solid fa-location-dot"></i> &nbsp; 40/3 Khu phố Bình Giao, Phường Thuận Giao, Thành phố Thuận An, Tỉnh Bình Dương</span>
           </p>
-          <p><img src="/anhduong/images/Phone.svg" alt="phone"><span>0388923998</span></p>
-          <p><img src="/anhduong/images/mail.svg" alt="mail"><span>contact@anhduongmetal.com.vn</span></p>
+          <p><span><i class="fa-solid fa-headphones"></i> &nbsp; 0388.923.998</span></p>
+          <p><span><i class="fa-solid fa-envelope"></i> &nbsp; contact@anhduongmetal.com.vn</span></p>
         </div>
         <div>
           <p class="custom-footer-address-title">Nhà xưởng</p>
-          <div style="width: 50px; height: 3px; background-color: #ffffff; margin-bottom:20px"></div>
-          <p>
-           <img src="/anhduong/images/location.svg">
-           <span>40/3 Khu phố Bình Giao, Phường Thuận Giao, Thành phố Thuận An, Tỉnh Bình Dương</span>
+          <div style="width: 150px; height: 3px; background-color: #ffffff; margin-bottom:20px"></div>
+          <p>           	
+            <span><i class="fa-solid fa-location-dot"></i> &nbsp; 40/3 Khu phố Bình Giao, Phường Thuận Giao, Thành phố Thuận An, Tỉnh Bình Dương</span>
           </p>
-          <p><img src="/anhduong/images/Phone.svg" alt="phone"><span>0388923998</span></p>
-          <p><img src="/anhduong/images/mail.svg" alt="mail"><span>contact@anhduongmetal.com.vn</span></p>
+          <p><span><i class="fa-solid fa-headphones"></i> &nbsp; 0388.923.998</span></p>
+          <p><span><i class="fa-solid fa-envelope"></i> &nbsp; contact@anhduongmetal.com.vn</span></p>
         </div>
         <div>
           
