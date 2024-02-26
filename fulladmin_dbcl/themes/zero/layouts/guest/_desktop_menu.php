7d9a56388c8d5c19ed488dc99b6d410bdee856b7
tyan-nguyen <nguyenvantyan@gmail.com>
2024-02-26 10:36:16 +0700

    update

---
+++ fulladmin_dbcl/themes/zero/layouts/guest/_desktop_menu.php
@@ -0,0 +1,92 @@
+<div class="header-menu header-menu-basic">
+    <div class="container">
+        <div class="header-menu-wrap">
+            <div class="header-menu-root has-sub">
+                <a href="https://khaothi.tmu.edu.vn" class="menu-root ">
+                    <img src="https://khaothi.tmu.edu.vn/template_dhtm/images/home_logo.png" alt="" class="img-fluid logo-head" />
+                </a></div>
+            <div class="header-menu-root  has-sub ">
+                <a href="https://khaothi.tmu.edu.vn/trang/gioi-thieu-130" class="menu-root                                                                         ">
+                    Giới thiệu<span class="icon-dropdown ">
+                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M11.9465 5.45337H7.79316H4.05317C3.41317 5.45337 3.09317 6.2267 3.5465 6.68004L6.99983 10.1334C7.55317 10.6867 8.45317 10.6867 9.0065 10.1334L10.3198 8.82003L12.4598 6.68004C12.9065 6.2267 12.5865 5.45337 11.9465 5.45337Z" fill="white" />
+                        </svg>
+                    </span>
+                </a>
+                <div class="sub-menu">
+                    <div class="menu-child">
+                        <a href="https://khaothi.tmu.edu.vn/trang/gioi-thieu-chung-130" class="menu-root lv1                                                                 ">Giới thiệu chung</a></div>
+                    <div class="menu-child">
+                        <a href="https://khaothi.tmu.edu.vn/trang/chuc-nang-nhiem-vu-131" class="menu-root lv1                                                                 ">Chức năng - Nhiệm vụ</a></div>
+                    <div class="menu-child">
+                        <a href="https://khaothi.tmu.edu.vn/co-cau-to-chuc" class="menu-root lv1                                                                 ">Cơ cấu tổ chức</a></div>
+                </div>
+            </div>
+            <div class="header-menu-root">
+                <a href="https://khaothi.tmu.edu.vn/danh-muc/tin-tuc-su-kien-4090" class="menu-root  ">
+                    Tin tức - Sự kiện
+                </a></div>
+            <!-- <div class="header-menu-root  has-sub ">
+                    <a href="https://khaothi.tmu.edu.vn/danh-muc/tin-tuc-khao-thi-2926" class="menu-root                                                                         ">
+                        Tin tức khảo thí<span class="icon-dropdown ">
+                            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M11.9465 5.45337H7.79316H4.05317C3.41317 5.45337 3.09317 6.2267 3.5465 6.68004L6.99983 10.1334C7.55317 10.6867 8.45317 10.6867 9.0065 10.1334L10.3198 8.82003L12.4598 6.68004C12.9065 6.2267 12.5865 5.45337 11.9465 5.45337Z" fill="white" />
+                            </svg>
+                        </span>
+                    </a>
+                    <div class="sub-menu">
+                        <div class="menu-child">
+                            <a href="https://khaothi.tmu.edu.vn/danh-muc/tin-tuc-khao-thi-2926?cat_child=2927" class="menu-root lv1    ">Tin hoạt động</a></div>
+                        <div class="menu-child">
+                            <a href="https://khaothi.tmu.edu.vn/danh-muc/tin-tuc-khao-thi-2926?cat_child=2928" class="menu-root lv1    ">Thông báo</a></div>
+                    </div>
+                </div> -->
+            <div class="header-menu-root  has-sub ">
+                <a href="https://khaothi.tmu.edu.vn/danh-muc/tin-tuc-dam-bao-chat-luong-2929" class="menu-root                                                                         ">
+                    Đảm bảo chất lượng<span class="icon-dropdown ">
+                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M11.9465 5.45337H7.79316H4.05317C3.41317 5.45337 3.09317 6.2267 3.5465 6.68004L6.99983 10.1334C7.55317 10.6867 8.45317 10.6867 9.0065 10.1334L10.3198 8.82003L12.4598 6.68004C12.9065 6.2267 12.5865 5.45337 11.9465 5.45337Z" fill="white" />
+                        </svg>
+                    </span>
+                </a>
+                <div class="sub-menu">
+                    <div class="menu-child  has-sub ">
+                        <a href="https://khaothi.tmu.edu.vn/danh-muc/tin-tuc-dam-bao-chat-luong-2929?cat_child=4315" class="menu-root lv1    ">
+                            Kiểm định chất lượng giáo dục
+                            <span class="icon-dropdown rotate-90">
+                                <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M11.9465 5.45337H7.79316H4.05317C3.41317 5.45337 3.09317 6.2267 3.5465 6.68004L6.99983 10.1334C7.55317 10.6867 8.45317 10.6867 9.0065 10.1334L10.3198 8.82003L12.4598 6.68004C12.9065 6.2267 12.5865 5.45337 11.9465 5.45337Z" fill="white" />
+                                </svg>
+                            </span>
+                        </a>
+                        <div class="sub-menu">
+                            <div class="menu-child">
+                                <a href="https://khaothi.tmu.edu.vn/danh-muc/kiem-dinh-chat-luong-giao-duc-4315?cat_child=4316" class="menu-root lv2  ">Kiểm định chất lượng Trường </a></div>
+                            <div class="menu-child">
+                                <a href="https://khaothi.tmu.edu.vn/danh-muc/kiem-dinh-chat-luong-giao-duc-4315?cat_child=4317" class="menu-root lv2  ">Kiểm định chất lượng CTĐT theo tiêu chuẩn MOET </a></div>
+                            <div class="menu-child">
+                                <a href="https://khaothi.tmu.edu.vn/danh-muc/kiem-dinh-chat-luong-giao-duc-4315?cat_child=4318" class="menu-root lv2  ">Kiểm định chất lượng CTĐT theo tiêu chuẩn AUN-QA </a></div>
+                        </div>
+                    </div>
+                    <div class="menu-child">
+                        <a href="https://khaothi.tmu.edu.vn/danh-muc/tin-tuc-dam-bao-chat-luong-2929?cat_child=2924" class="menu-root lv1    ">Tin hoạt động</a></div>
+                    <div class="menu-child">
+                        <a href="https://khaothi.tmu.edu.vn/danh-muc/tin-tuc-dam-bao-chat-luong-2929?cat_child=2925" class="menu-root lv1    ">Thông báo</a></div>
+                        
+                        <?= $this->render('_menuItem') ?>
+                        
+                        
+                        
+                </div>
+            </div>
+            <div class="header-menu-root">
+                <a href="https://khaothi.tmu.edu.vn/danh-muc/quan-ly-phoi-vbcc-4091" class="menu-root  ">
+                    Quản lý phôi VBCC
+                </a></div>
+            <div class="header-menu-root">
+                <a href="https://khaothi.tmu.edu.vn/van-ban-quan-ly" class="menu-root  ">
+                    Văn bản quản lý
+                </a></div>
+        </div>
+    </div>
+</div>
---
+++ fulladmin_dbcl/themes/zero/layouts/guest/_menuItem.php
@@ -0,0 +1,37 @@
+<div class="menu-child  has-sub ">
+    <a href="#" class="menu-root lv1    ">
+        Test 1
+        <span class="icon-dropdown rotate-90">
+            <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M11.9465 5.45337H7.79316H4.05317C3.41317 5.45337 3.09317 6.2267 3.5465 6.68004L6.99983 10.1334C7.55317 10.6867 8.45317 10.6867 9.0065 10.1334L10.3198 8.82003L12.4598 6.68004C12.9065 6.2267 12.5865 5.45337 11.9465 5.45337Z" fill="white" />
+            </svg>
+        </span>
+    </a>
+    <div class="sub-menu">
+        <div class="menu-child">
+            <a href="#" class="menu-root lv2  ">tes 1 </a></div>
+        <div class="menu-child">
+            <a href="#" class="menu-root lv2  ">Test 2 </a></div>
+            
+        <div class="menu-child has-sub">
+                    <a href="#" class="menu-root lv1    ">
+                    Test 3
+                    <span class="icon-dropdown rotate-90">
+                        <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M11.9465 5.45337H7.79316H4.05317C3.41317 5.45337 3.09317 6.2267 3.5465 6.68004L6.99983 10.1334C7.55317 10.6867 8.45317 10.6867 9.0065 10.1334L10.3198 8.82003L12.4598 6.68004C12.9065 6.2267 12.5865 5.45337 11.9465 5.45337Z" fill="white" />
+                        </svg>
+                    </span>
+                </a>
+                <div class="sub-menu">
+                    <div class="menu-child">
+                        <a href="#" class="menu-root lv2">tes 5 </a></div>
+                    <div class="menu-child">
+                        <a href="#" class="menu-root lv2">Test 6 </a></div>
+                        
+                    <div class="menu-child has-sub">
+                        <a href="#" class="menu-root lv2">Test 7 </a>
+                    </div>
+                </div>
+        </div>
+    </div>
+</div>
